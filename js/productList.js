00094f646caf0d4edcdcc48d94ed8db773ac4b05
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-20 17:40:28 +0200

    feat: added the functionality of brand, color, price, condition and sort buttons

--- js/productList.js
+++ js/productList.js
@@ -1,116 +1,178 @@
 let products = [];
 
 async function fetchProducts() {
-  try {
-    const response = await fetch('../utils/products.json');
-    const data = await response.json();
-    products = data.products;
-    getFiltersData();
-    renderProducts();
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
+	try {
+		const response = await fetch('../utils/products.json');
+		const data = await response.json();
+		products = data.products;
+		getFiltersData();
+		renderProducts();
+		document.querySelectorAll("#brandFilter").forEach(element => {
+			element.addEventListener('click', (e) => {
+				const brandValue = element.querySelector("input").value;
+				products = products.filter(prod => prod.brand === brandValue);
+				renderProducts();
+			})
+		});
+		document.querySelectorAll("#colorFilter").forEach(element => {
+			element.addEventListener('click', (e) => {
+				const colorValue = element.querySelector("input").value;
+				products = products.filter(prod => prod.color === colorValue);
+				renderProducts();
+			})
+		});
+		document.querySelectorAll("#priceFilter li").forEach(element => {
+			element.addEventListener('click', (e) => {
+				const priceValue = element.querySelector("input").value;
+				products = products.filter(({ price }) => {
+					if (priceValue === "under") {
+						return price < 110;
+					} else if (priceValue === "inBetween") {
+						return price >= 110 && price <= 180;
+					} else if (priceValue === "above") {
+						return price > 180;
+					}
+				});
+				renderProducts();
+			})
+		});
+		document.querySelectorAll("#conditionFilter li").forEach(element => {
+			element.addEventListener('click', (e) => {
+				const conditionValue = element.querySelector("input").value;
+				products = products.filter(({ condition }) => condition === conditionValue);
+				renderProducts();
+			})
+		});
+		document.querySelectorAll("#sortFilter li").forEach(element => {
+			element.addEventListener('click', (e) => {
+				const sortValue = element.querySelector("input").value;
+				if (sortValue === "Name (A-Z)") {
+					products.sort((a, b) => {
+						const nameA = a.name.toUpperCase(); // Convert names to uppercase for case-insensitive comparison
+						const nameB = b.name.toUpperCase();
+
+						if (nameA < nameB) {
+							return -1;
+						}
+						if (nameA > nameB) {
+							return 1;
+						}
+						return 0; // Names are equal
+					});
+				} else {
+					products.sort((a, b) => {
+						const nameA = a.name.toLowerCase();
+						const nameB = b.name.toLowerCase();
+						if (nameA > nameB) {
+							return -1;
+						}
+						if (nameA < nameB) {
+							return 1;
+						}
+						return 0;
+					});
+				}
+				renderProducts();
+			}
+			)
+		});
+	} catch (error) {
+		console.error('Error fetching data:', error);
+	}
 }
 
 async function init() {
-  await fetchProducts();
-}
-
-const renderProducts = () => {
-  const productsContainer = document.querySelector(".productsContainer");
-  if (!productsContainer) {
-    console.error('Products container not found');
-    return;
-  }
-
-  productsContainer.innerHTML = '';
-
-  products.forEach(({ id, name, price, shipping, watching, isSponsored, imageUrl }) => {
-    const productElement = createProductElement(id, name, price, shipping, watching, isSponsored, imageUrl);
-    productsContainer.appendChild(productElement);
-  });
+	await fetchProducts();
 }
 
 const createProductElement = (id, name, price, shipping, watching, isSponsored, imageUrl) => {
-  const productElement = document.createElement('div');
-  productElement.classList.add('col');
-  productElement.id = id;
+	const productElement = document.createElement('div');
+	productElement.classList.add('col');
+	productElement.id = id;
 
-  productElement.innerHTML = `
+	productElement.innerHTML = `
     <article class="card h-100">
         <img src="${imageUrl}" class="card-img-top img-thumbnail" alt="${name}">
         <div class="card-body">
             <h5 class="card-title">${name}</h5>
             <div class="card-text">
-                <span class="fw-bold">${price}</span>
-                <span class="text-muted d-block">${shipping} shipping</span>
-                <span class="text-danger d-block">${watching} watching</span>
+                <span class="d-block mb-1 fw-bold">$${price}</span>
+                <span class="d-block mb-1 text-muted d-block">${shipping} shipping</span>
+                <span class="d-block mb-1 text-danger">${watching} watching</span>
                 <p class="mt-2 mb-0 text-muted">${isSponsored ? 'SPONSORED' : ''}</p>
             </div>
         </div>
     </article>
   `;
 
-  return productElement;
+	return productElement;
+}
+
+const renderProducts = () => {
+	const productsContainer = document.querySelector(".productsContainer");
+	if (!productsContainer) {
+		console.error('Products container not found');
+		return;
+	}
+
+	productsContainer.innerHTML = '';
+
+	products.forEach(({ id, name, price, shipping, watching, isSponsored, imageUrl }) => {
+		const productElement = createProductElement(id, name, price, shipping, watching, isSponsored, imageUrl);
+		productsContainer.appendChild(productElement);
+	});
 }
 
 const getFiltersData = () => {
-  let brandSet = new Set([]);
-  let colorSet = new Set([]);
-  let conditionSet = new Set([]);
-  products.forEach(({ brand, color, condition }) => {
-    brandSet.add(brand);
-    colorSet.add(color);
-    conditionSet.add(condition);
-  })
-  renderFilterData(brandSet, colorSet, conditionSet);
-  console.log(brandSet);
-  console.log(colorSet);
-  console.log(conditionSet);
+	let brandSet = new Set([]);
+	let colorSet = new Set([]);
+	let conditionSet = new Set([]);
+	products.forEach(({ brand, color, condition }) => {
+		brandSet.add(brand);
+		colorSet.add(color);
+		conditionSet.add(condition);
+	})
+	renderFilterData(brandSet, colorSet, conditionSet);
 }
 
 const createFilterElement = (item, isColor) => {
 
-  const listItemFilter = document.createElement('li');
-  if (isColor) {
-    listItemFilter.innerHTML = `
-    <li>
+	const listItemFilter = document.createElement('li');
+	if (isColor) {
+		listItemFilter.innerHTML = `
       <a class="dropdown-item" href="#">
         <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="${item}" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
+            <input class="form-check-input" type="checkbox" value="${item}" id="flexCheckDefault${item}">
+            <label class="form-check-label" for="flexCheckDefault${item}">
               <span class="rounded-circle d-inline-block colorOptions" style="background-color: ${item};"></span>
             </label>
         </div>
       </a>
-    </li>
   `;
-  } else {
-    listItemFilter.innerHTML = `
-    <li>
-      <a class="dropdown-item" href="#">
+	} else {
+		listItemFilter.innerHTML = `
+      <a class="dropdown-item" href="#" for="flexCheckDefault${item}">
         <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="${item}" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
+            <input class="form-check-input" type="checkbox" name="${item}" value="${item}" id="flexCheckDefault${item}">
+            <label class="form-check-label" for="flexCheckDefault${item}">
                 ${item}
             </label>
         </div>
       </a>
-    </li>
   `;
-  }
-  return listItemFilter;
+	}
+	return listItemFilter;
 }
 
 const renderFilterData = (brandSet, colorSet, conditionSet) => {
-  const brandFilter = document.querySelector(".brand ul");
-  brandSet.forEach((brand) => brandFilter.appendChild(createFilterElement(brand, false)));
+	const brandFilter = document.querySelector(".brand ul");
+	brandSet.forEach((brand) => brandFilter.appendChild(createFilterElement(brand, false)));
 
-  const conditionFilter = document.querySelector(".condition ul");
-  conditionSet.forEach((condition) => conditionFilter.appendChild(createFilterElement(condition, false)));
+	const conditionFilter = document.querySelector(".condition ul");
+	conditionSet.forEach((condition) => conditionFilter.appendChild(createFilterElement(condition, false)));
 
-  const colorFilter = document.querySelector(".color ul");
-  colorSet.forEach((color) => colorFilter.appendChild(createFilterElement(color, true)));
+	const colorFilter = document.querySelector(".color ul");
+	colorSet.forEach((color) => colorFilter.appendChild(createFilterElement(color, true)));
 }
 
 const filterProducts = () => {
@@ -118,3 +180,5 @@ const filterProducts = () => {
 }
 
 init();
+
+
