df6abaeb8a7b48e3706db0b22d5fa852423126ba
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-03-21 02:19:17 +0200

    feat: add shipping, sponsred, view functionality filter

--- js/productList.js
+++ js/productList.js
@@ -5,6 +5,7 @@ async function fetchProducts() {
 		const response = await fetch('../utils/products.json');
 		const data = await response.json();
 		products = data.products;
+		const unfilteredProducts = products;
 		getFiltersData();
 		renderProducts();
 		document.querySelectorAll("#brandFilter").forEach(element => {
@@ -14,6 +15,7 @@ async function fetchProducts() {
 				renderProducts();
 			})
 		});
+
 		document.querySelectorAll("#colorFilter").forEach(element => {
 			element.addEventListener('click', (e) => {
 				const colorValue = element.querySelector("input").value;
@@ -21,6 +23,7 @@ async function fetchProducts() {
 				renderProducts();
 			})
 		});
+
 		document.querySelectorAll("#priceFilter li").forEach(element => {
 			element.addEventListener('click', (e) => {
 				const priceValue = element.querySelector("input").value;
@@ -36,6 +39,7 @@ async function fetchProducts() {
 				renderProducts();
 			})
 		});
+
 		document.querySelectorAll("#conditionFilter li").forEach(element => {
 			element.addEventListener('click', (e) => {
 				const conditionValue = element.querySelector("input").value;
@@ -43,6 +47,7 @@ async function fetchProducts() {
 				renderProducts();
 			})
 		});
+
 		document.querySelectorAll("#sortFilter li").forEach(element => {
 			element.addEventListener('click', (e) => {
 				const sortValue = element.querySelector("input").value;
@@ -76,6 +81,65 @@ async function fetchProducts() {
 			}
 			)
 		});
+
+		document.querySelectorAll("#shippingFilter li").forEach(element => {
+			element.addEventListener('click', (e) => {
+				const shippingValue = element.querySelector("input").value;
+				products = products.filter(({ shipping }) => {
+					if (shippingValue == 0) {
+						return shipping === 0;
+					} else if (shippingValue == 15) {
+						return shipping === 15;
+					} else {
+						return shipping > 15;
+					}
+				});
+				renderProducts();
+			})
+		});
+
+		document.querySelectorAll("#sponsoredFilter li").forEach(element => {
+			element.addEventListener('click', (e) => {
+				const sponsoredValue = element.querySelector("input").value;
+				products = products.filter(({ isSponsored }) => {
+					if (sponsoredValue == "sponsored") {
+						return isSponsored === true;
+					} else {
+						return !isSponsored;
+					}
+				});
+				renderProducts();
+			})
+		});
+
+		document.querySelectorAll("#viewFilter li").forEach(element => {
+			element.addEventListener('click', (e) => {
+				const viewValue = element.querySelector("input").value ?? 'grid';
+				const productsContainer = document.querySelector(".productsContainer");
+				const horizontalProductsContainer = document.querySelector(".horizontalContainer");
+				if (viewValue === 'grid') {
+					productsContainer.classList.remove("d-none");
+					horizontalProductsContainer.classList.add("d-none");
+					return;
+				}
+				productsContainer.classList.add("d-none");
+				horizontalProductsContainer.classList.remove("d-none");
+				renderProducts();
+			})
+		});
+
+		const clearBtn = document.querySelector(".clearBtn button");
+		clearBtn.addEventListener('click', () => {
+			products = unfilteredProducts;
+			document.querySelectorAll(".filter-section div").forEach(elem => {
+				elem.querySelectorAll("input").forEach(inp => inp.checked = false);
+			});
+			document.querySelector(".productsContainer").classList.remove("d-none");
+			document.querySelector(".horizontalContainer").classList.add("d-none");
+			console.log(unfilteredProducts);
+			console.log(products);
+			renderProducts();
+		})
 	} catch (error) {
 		console.error('Error fetching data:', error);
 	}
@@ -97,7 +161,7 @@ const createProductElement = (id, name, price, shipping, watching, isSponsored, 
             <h5 class="card-title">${name}</h5>
             <div class="card-text">
                 <span class="d-block mb-1 fw-bold">$${price}</span>
-                <span class="d-block mb-1 text-muted d-block">${shipping} shipping</span>
+                <span class="d-block mb-1 text-muted d-block">${shipping ? "$" + shipping : 'Free shipping'}</span>
                 <span class="d-block mb-1 text-danger">${watching} watching</span>
                 <p class="mt-2 mb-0 text-muted">${isSponsored ? 'SPONSORED' : ''}</p>
             </div>
@@ -108,18 +172,56 @@ const createProductElement = (id, name, price, shipping, watching, isSponsored, 
 	return productElement;
 }
 
+const createHorizontalProduct = (id, name, price, shipping, watching, isSponsored, imageUrl) => {
+	const productElement = document.createElement('div');
+	productElement.classList.add('card');
+	productElement.classList.add('my-4');
+	productElement.id = id;
+	productElement.innerHTML = `
+		<div class="row g-0">
+			<div class="col-4">
+				<img src="${imageUrl}"
+					class="img-fluid rounded-start" alt="${name}" />
+			</div>
+			<div class="col-8">
+				<div class="card-body">
+					<h5 class="card-title fs-5 fs-lg-4">
+						${name}
+					</h5>
+					<div class="card-text">
+						<span class="fw-bold fs-6 fs-lg-5">$${price}</span>
+						<span class="text-muted d-block small">${shipping ? "$" + shipping : 'Free shipping'}</span>
+						<span class="text-danger d-block small">${watching} watching</span>
+						<p class="mt-2 mb-0 text-muted small">${isSponsored ? 'SPONSORED' : ''}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	`;
+	return productElement;
+}
+
 const renderProducts = () => {
 	const productsContainer = document.querySelector(".productsContainer");
+	const horizontalProductsContainer = document.querySelector(".horizontalContainer");
 	if (!productsContainer) {
 		console.error('Products container not found');
 		return;
 	}
 
+	if (!horizontalProductsContainer) {
+		console.error('Products container not found');
+		return;
+	}
+
 	productsContainer.innerHTML = '';
+	horizontalProductsContainer.innerHTML = '';
 
 	products.forEach(({ id, name, price, shipping, watching, isSponsored, imageUrl }) => {
 		const productElement = createProductElement(id, name, price, shipping, watching, isSponsored, imageUrl);
 		productsContainer.appendChild(productElement);
+		const horizontalProductElement = createHorizontalProduct(id, name, price, shipping, watching, isSponsored, imageUrl);
+		horizontalProductsContainer.appendChild(horizontalProductElement);
 	});
 }
 
@@ -140,26 +242,26 @@ const createFilterElement = (item, isColor) => {
 	const listItemFilter = document.createElement('li');
 	if (isColor) {
 		listItemFilter.innerHTML = `
-      <a class="dropdown-item" href="#">
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="${item}" id="flexCheckDefault${item}">
-            <label class="form-check-label" for="flexCheckDefault${item}">
-              <span class="rounded-circle d-inline-block colorOptions" style="background-color: ${item};"></span>
-            </label>
-        </div>
-      </a>
-  `;
+		<a class="dropdown-item" href="#">
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="${item}" id="flexCheckDefault${item}">
+				<label class="form-check-label" for="flexCheckDefault${item}">
+				<span class="rounded-circle d-inline-block colorOptions" style="background-color: ${item};"></span>
+				</label>
+			</div>
+		</a>
+`;
 	} else {
 		listItemFilter.innerHTML = `
-      <a class="dropdown-item" href="#" for="flexCheckDefault${item}">
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" name="${item}" value="${item}" id="flexCheckDefault${item}">
-            <label class="form-check-label" for="flexCheckDefault${item}">
-                ${item}
-            </label>
-        </div>
-      </a>
-  `;
+		<a class="dropdown-item" href="#" for="flexCheckDefault${item}">
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" name="${item}" value="${item}" id="flexCheckDefault${item}">
+				<label class="form-check-label" for="flexCheckDefault${item}">
+					${item}
+				</label>
+			</div>
+		</a>
+`;
 	}
 	return listItemFilter;
 }
